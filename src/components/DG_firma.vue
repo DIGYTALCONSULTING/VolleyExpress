01ca3fa8a4631c843d869576970f96d591fa973e
DIGYTALCONSULTING <digytalconsulting@outlook.com>
2025-09-24 17:26:49 -0500

    deploy: usar /docs para GitHub Pages (+404, +.nojekyll)

---
+++ src/components/DG_firma.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+   <a class="text-center text-2xl font-bold text-white">
+          PROCESO DE PEDIDO
+   </a>
+</div>
+      
+      
+</template>
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  // 👇 MUY IMPORTANTE para GitHub Pages: usa el nombre EXACTO del repo
-  base: '/VolleyExpressShop/',
+  // 👇 nombre EXACTO del repo (respeta mayúsculas)
+  base: '/VolleyExpress/',
+  // 👇 GitHub Pages leerá /docs
+  build: { outDir: 'docs' },
   plugins: [
     vue(),
     tailwindcss(),
