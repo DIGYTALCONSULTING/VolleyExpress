988058608edcc48aaf0d2943241b265646585407
DIGYTALCONSULTING <digytalconsulting@outlook.com>
2025-09-24 15:46:39 -0500

    primer commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  // 👇 MUY IMPORTANTE para GitHub Pages: usa el nombre EXACTO del repo
+  base: '/VolleyExpressShop/',
   plugins: [
     vue(),
     tailwindcss(),
